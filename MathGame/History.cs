1c4dd472dde86261c00547c8360a52a947176a56
Kiyosuki <kiyosuki@hotmail.com>
2025-03-05 00:06:41 +0600

    Modified History.cs

--- MathGame/History.cs
+++ MathGame/History.cs
@@ -2,5 +2,27 @@
 {
     class History
     {
+        private static List<string> gameHistory = new List<string>();
+
+        public static void AddResult(string result)
+        {
+            gameHistory.Add(result);
+        }
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("--- Game History ---");
+            if (gameHistory.Count == 0)
+            {
+                Console.WriteLine("Game History is empty.");
+            }
+            else
+            {
+                foreach (string gameResult in gameHistory)
+                {
+                    Console.WriteLine(gameResult);
+                }
+            }
+        }
     }
 }
