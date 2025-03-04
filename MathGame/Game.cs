47f0c7d625a329248fad67d26a303792ee4e410a
Kiyosuki <kiyosuki@hotmail.com>
2025-03-05 00:21:38 +0600

    Modified Game.cs

--- MathGame/Game.cs
+++ MathGame/Game.cs
@@ -1,7 +1,82 @@
-﻿namespace MathGame
+﻿using System.Diagnostics;
+
+namespace MathGame
 {
     class Game
     {
+        public static void StartGame(string operation)
+        {
+            Random random = new Random();
+            int score = 0;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
 
+            for (int i = 0; i < 5; i++)
+            {
+                (int num1, int num2, int correctAnswer, string question) = GenerateQuestion(operation, random);
+
+                Console.WriteLine($"Question {i}: {question}");
+                int userAnswer = Utils.GetInteger("Your answer: ");
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. The correct answer was: {correctAnswer}");
+                }
+
+                timer.Stop();
+                TimeSpan elapsedTime = timer.Elapsed;
+                string result = $"Game: {operation} | Score: {score}/5 | Time: {elapsedTime.Seconds} sec.";
+                History.AddResult(result);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        private static (int, int, int, string) GenerateQuestion(string operation, Random random)
+        {
+            int num1, num2, correctAnswer;
+            string question;
+
+            if (operation == "random")
+            {
+                string[] operations = { "+", "-", "*", "/" };
+                operation = operations[random.Next(operations.Length)];
+            }
+
+            do
+            {
+                num1 = random.Next(0, 101);
+                num2 = random.Next(1, 101);
+            } while (operation == "/" && num1 % num2 != 0);
+
+            switch (operation)
+            {
+                case "+":
+                    correctAnswer = num1 + num2;
+                    question = $"{num1} {operation} {num2} = ?";
+                    break;
+                case "-":
+                    correctAnswer = num1 - num2;
+                    question = $"{num1} {operation} {num2} = ?";
+                    break;
+                case "*":
+                    correctAnswer = num1 * num2;
+                    question = $"{num1} {operation} {num2} = ?";
+                    break;
+                case "/":
+                    correctAnswer = num1 / num2;
+                    question = $"{num1} {operation} {num2} = ?";
+                    break;
+                default:
+                    throw new Exception("Unkown operation.");
+            }
+
+            return (num1, num2, correctAnswer, question);
+        }
     }
 }
