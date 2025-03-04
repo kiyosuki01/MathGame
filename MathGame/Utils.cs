1fa1afb961d470f9c1c85f0e509de87f97d6f0dc
Kiyosuki <kiyosuki@hotmail.com>
2025-03-04 23:58:08 +0600

    Created a new method to get numbers

--- MathGame/Utils.cs
+++ MathGame/Utils.cs
@@ -2,6 +2,16 @@
 {
     class Utils
     {
-
+        internal static int GetInteger(string message)
+        {
+            int number;
+            while (true)
+            {
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out number))
+                    return number;
+                Console.WriteLine("Invalid input. Enter a number: ");
+            }
+        }
     }
 }
