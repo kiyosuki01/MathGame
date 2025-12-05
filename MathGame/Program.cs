3f53f39e8036a95d9f9d5e6645c85c6182a9518c
kiyosuki <kiyosuki@hotmail.com>
2025-12-05 17:42:13 +0500

    Add main menu structure

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿class Program
+{
+    static void Main()
+    {
+        MainMenu();
+    }
+
+    static void MainMenu()
+    {
+        while (true)
+        {
+            Console.WriteLine("--- Math Game ---");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("-----------------");
+            Console.Write("Your choice: ");
+
+            string? userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "0":
+                    return;
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
